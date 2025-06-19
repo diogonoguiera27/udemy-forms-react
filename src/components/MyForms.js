aa26ad6fe48a01385cdbc13811e5409589b21c4c
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-19 10:35:39 -0300

    Label Envolvendo Input outra Forma de Cria Formulario

--- src/components/MyForms.js
+++ src/components/MyForms.js
@@ -9,6 +9,11 @@ const MyForms = () => {
                 <label htmlFor="name">nome:</label>
                 <input type='text' name='name' placeholder='Digite o Seu Nome'/>
             </div>
+            {/* 2 - label envolvendo input */}
+            <label >
+                <span>Email:</span>
+                <input type="text" name='email' placeholder='Digite o seu e-mail'/>
+            </label>
             <input type='submit' value="Enviar"/>
         </form>
     </div>
