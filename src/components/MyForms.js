d7559403e408203dc3a64bd9123d7620e2927f7d
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-19 10:47:40 -0300

    Gerenciamento de dados Input

--- src/components/MyForms.js
+++ src/components/MyForms.js
@@ -1,23 +1,37 @@
-import './MyForms.css'
+import { useState } from "react";
+import "./MyForms.css";
 
 const MyForms = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+   setName(e.target.value);
+  };
+  console.log(name)
+  
   return (
     <div>
-        {/* Criação de Forms */}
-        <form>
-            <div>
-                <label htmlFor="name">nome:</label>
-                <input type='text' name='name' placeholder='Digite o Seu Nome'/>
-            </div>
-            {/* 2 - label envolvendo input */}
-            <label >
-                <span>Email:</span>
-                <input type="text" name='email' placeholder='Digite o seu e-mail'/>
-            </label>
-            <input type='submit' value="Enviar"/>
-        </form>
+      {/* Criação de Forms */}
+      <form>
+        <div>
+          <label htmlFor="name">nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o Seu Nome"
+            onChange={handleName}
+          />
+        </div>
+        {/* 2 - label envolvendo input */}
+        <label>
+          <span>Email:</span>
+          <input type="text" name="email" placeholder="Digite o seu e-mail" />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForms
+export default MyForms;
